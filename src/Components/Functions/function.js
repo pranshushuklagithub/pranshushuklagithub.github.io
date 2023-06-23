f174199cbd0f4c4b9c9ec5296116b333ea0e4daf
Pranshu Shukla <pranshushuklaoffice@gmail.com>
2023-06-24 04:55:34 +0530

    setting

--- src/Components/Functions/function.js
+++ src/Components/Functions/function.js
@@ -1,12 +1,12 @@
 export const downloadResume=()=>{
-    fetch('Pranshu_Shukla_Resume.pdf').then(response => {
+    fetch('.Pranshu_Shukla_Resume.pdf').then(response => {
         response.blob().then(blob => {
             // Creating new object of PDF file
             const fileURL = window.URL.createObjectURL(blob);
             // Setting various property values
             let alink = document.createElement('a');
             alink.href = fileURL;
-            alink.download = 'PranshuCV.pdf';
+            alink.download = 'Pranshu_Shukla_Resume.pdf';
             alink.click();
         })
     })
