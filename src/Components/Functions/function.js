efe84e9bbbc6fc2c40757fed830438373cad5d60
Pranshu Shukla <pranshushuklaoffice@gmail.com>
2023-06-24 21:41:42 +0530

    set

--- src/Components/Functions/function.js
+++ src/Components/Functions/function.js
@@ -7,7 +7,7 @@ export const downloadResume=()=>{
     window.open("https://drive.google.com/file/d/1wOhjFp3m5wa1uqpqTEeIPV8iWZkaKb7h/view?usp=sharing","_blank")
     const fileURL = "https://drive.google.com/uc?export=download&id=1wOhjFp3m5wa1uqpqTEeIPV8iWZkaKb7h";
             
-           console.log("hash")
+          //  console.log("hash")
              // Setting various property values
             let alink = document.createElement('a');
             alink.href = fileURL;
