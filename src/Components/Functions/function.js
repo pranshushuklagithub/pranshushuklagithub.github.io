60559b8d46a3198317dce6986533ed86008267ad
Pranshu Shukla <pranshushuklaoffice@gmail.com>
2023-06-24 06:33:33 +0530

    setting up

--- src/Components/Functions/function.js
+++ src/Components/Functions/function.js
@@ -1,7 +1,7 @@
 export const downloadResume=()=>{
     window.open("./Pranshu_Shukla_Resume.pdf")
-    const fileURL = "./Pranshu_Shukla_Resume.pdf";
-            console.log(fileURL)
+    const fileURL = "https://drive.google.com/file/d/1wOhjFp3m5wa1uqpqTEeIPV8iWZkaKb7h/view?usp=sharing";
+            console.log()
             // Setting various property values
             let alink = document.createElement('a');
             alink.href = fileURL;
