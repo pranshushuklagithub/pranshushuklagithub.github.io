58058acc84d17158e1f8e2d60b7348858276f7a7
Pranshu Shukla <pranshushuklaoffice@gmail.com>
2023-06-29 21:14:33 +0530

    set resume

--- src/Components/Functions/function.js
+++ src/Components/Functions/function.js
@@ -1,11 +1,11 @@
 
 
-// https://drive.google.com/file/d/1wOhjFp3m5wa1uqpqTEeIPV8iWZkaKb7h/view?usp=sharing        this is direct google drive link 
+// https://drive.google.com/file/d/1ses2VtkMl_8bzwViHhgXZi7P7fcfykTE/view?usp=sharing        this is direct google drive link 
 
-// https://drive.google.com/uc?export=download&id=1wOhjFp3m5wa1uqpqTEeIPV8iWZkaKb7h            this is download link from google drive
+// https://drive.google.com/uc?export=download&id=1ses2VtkMl_8bzwViHhgXZi7P7fcfykTE            this is download link from google drive
 export const downloadResume=()=>{
-    window.open("https://drive.google.com/file/d/1wOhjFp3m5wa1uqpqTEeIPV8iWZkaKb7h/view?usp=sharing","_blank")
-    const fileURL = "https://drive.google.com/uc?export=download&id=1wOhjFp3m5wa1uqpqTEeIPV8iWZkaKb7h";
+    window.open("https://drive.google.com/file/d/1ses2VtkMl_8bzwViHhgXZi7P7fcfykTE/view?usp=sharing","_blank")
+    const fileURL = "https://drive.google.com/uc?export=download&id=1ses2VtkMl_8bzwViHhgXZi7P7fcfykTE";
             
           //  console.log("hash")
              // Setting various property values
