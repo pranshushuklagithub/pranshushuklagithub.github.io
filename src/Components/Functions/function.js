26c67b3f6f64b080be78c49930be3cab00dafd53
Pranshu Shukla <pranshushuklaoffice@gmail.com>
2023-07-04 20:08:25 +0530

    setting up new resume

--- src/Components/Functions/function.js
+++ src/Components/Functions/function.js
@@ -2,6 +2,7 @@
 
 // https://drive.google.com/file/d/1ses2VtkMl_8bzwViHhgXZi7P7fcfykTE/view?usp=sharing        this is direct google drive link 
 
+
 // https://drive.google.com/uc?export=download&id=1ses2VtkMl_8bzwViHhgXZi7P7fcfykTE            this is download link from google drive
 export const downloadResume=()=>{
     window.open("https://drive.google.com/file/d/1ses2VtkMl_8bzwViHhgXZi7P7fcfykTE/view?usp=sharing","_blank")
