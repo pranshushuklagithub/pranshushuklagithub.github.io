1333e271ac6bde3762653efcf9cece02f4e1c847
Pranshu Shukla <pranshushuklaoffice@gmail.com>
2023-07-04 22:34:45 +0530

    set

--- src/Components/Functions/function.js
+++ src/Components/Functions/function.js
@@ -5,8 +5,8 @@
 
 // https://drive.google.com/uc?export=download&id=1ses2VtkMl_8bzwViHhgXZi7P7fcfykTE            this is download link from google drive
 export const downloadResume=()=>{
-    window.open("https://drive.google.com/file/d/1ses2VtkMl_8bzwViHhgXZi7P7fcfykTE/view","_blank")
-    const fileURL = "https://drive.google.com/uc?export=download&id=1ses2VtkMl_8bzwViHhgXZi7P7fcfykTE";
+    window.open("https://1drv.ms/b/s!AplsJENjsBMsuyJ223S9aVrNKEP5","_blank")
+    const fileURL = "https://1drv.ms/b/s!AplsJENjsBMsuyJ223S9aVrNKEP5&download=1";
             
           //  console.log("hash")
              // Setting various property values
