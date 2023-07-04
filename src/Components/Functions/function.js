c86782566138df884ccc81be8ea2624babba4911
Pranshu Shukla <pranshushuklaoffice@gmail.com>
2023-07-04 20:47:14 +0530

    set

--- src/Components/Functions/function.js
+++ src/Components/Functions/function.js
@@ -5,7 +5,7 @@
 
 // https://drive.google.com/uc?export=download&id=1ses2VtkMl_8bzwViHhgXZi7P7fcfykTE            this is download link from google drive
 export const downloadResume=()=>{
-    window.open("https://drive.google.com/file/d/1ses2VtkMl_8bzwViHhgXZi7P7fcfykTE/view?usp=sharing","_blank")
+    window.open("https://drive.google.com/file/d/1ses2VtkMl_8bzwViHhgXZi7P7fcfykTE/view","_blank")
     const fileURL = "https://drive.google.com/uc?export=download&id=1ses2VtkMl_8bzwViHhgXZi7P7fcfykTE";
             
           //  console.log("hash")
